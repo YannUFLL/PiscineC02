883210148aafe7728afb14f03518be01500900c2
Yann Dumaine <yannlescoul79f>
2022-01-21 13:15:47 +0100

    ex09 correction bug

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,47 +6,37 @@
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 13:04:39 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/01/20 22:21:40 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/01/21 13:14:46 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_cara(char *str, int i)
-{
-	int	u;
-
-	u = i;
-	while ((str[u] != 32) && (str[u] != 0))
-	{
-		if ((str[u] == 43) || (str[u] == 45))
-			break ;
-		else if ((str[u + 1] >= 65) && (str[u + 1] <= 90))
-			str[u + 1] = str[u + 1] + 32;
-		u++;
-	}
-	return (u);
-}
-
+#include <stdio.h>
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
-	int	c;
 
 	i = 0;
-	c = 1;
 	while (str[i] != '\0')
 	{
-		while (str[i] == 32)
-			i++;
 		if (((str[i] >= 97) && (str[i] <= 122)))
 		{
 			str[i] = str[i] - 32;
 		}
-		i = ft_cara(str, i) + i;
+		while ((str[i] != 32) && (str[i] != 0) && (str[i] != 59)) 
+		{
+			if ((str[i] == 43) || (str[i] == 45) || (str[i] <= 31))
+				break ;
+			if (str[i] == 127)
+				break ;
+			else if ((str[i + 1] >= 65) && (str[i + 1] <= 90))
+				str[i + 1] = str[i + 1] + 32;
+			i++;
+		}
 		i++;
 	}
 	return (str);
 }
-#include <stdio.h>
+
 int	main(void)
 {
 	char	str1[] = "hello";
@@ -54,7 +44,7 @@ int	main(void)
 	char	str3[] = "42helLO";
 	char	str4[] = "hELlo;woRLd";
 	char	str5[] = "\t$t@^/3{DF";
-	char	str6[] = "ReKt17+LOl MdR Mdr 4242l42";
+	char	str6[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
 	char	str7[] = "";
 
 	ft_strcapitalize(str1);
@@ -64,12 +54,13 @@ int	main(void)
 	ft_strcapitalize(str5);
 	ft_strcapitalize(str6);
 	ft_strcapitalize(str7);
-
-	printf("%s", str1);
-	printf("%s", str2);
-	printf("%s", str3);
-	printf("%s", str4);
-	printf("%s", str5);
-	printf("%s", str6);
-	printf("%s", str7);
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+	printf("%s\n", str3);
+	printf("%s\n", str4);
+	printf("%s\n", str5);
+	printf("%s\n", str6);
+	printf("%s\n", str7);
 }
+
+
