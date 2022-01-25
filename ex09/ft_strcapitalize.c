3c804252bc13a74bfab1ee81aa87a1ff89916158
Yann Dumaine <yannlescoul79f>
2022-01-25 18:28:25 +0100

    ex09 erreur d'une unitee dans la comparaison

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: ydumaine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 13:04:39 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/01/25 12:58:13 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/01/25 18:27:38 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	*ft_strcapitalize(char *str)
 		{
 			str[i] = str[i] - 32;
 		}
-		while ((str[i] >= 47) && (str[i] <= 122))
+		while ((str[i] >= 48) && (str[i] <= 122))
 		{
 			if (str[i] >= 58 && str[i] <= 64)
 				break ;
-			if (str[i] >= 91 && str[i] <= 97)
+			if (str[i] >= 91 && str[i] <= 96)
 				break ;
 			else if ((str[i + 1] >= 65) && (str[i + 1] <= 90))
 				str[i + 1] = str[i + 1] + 32;
